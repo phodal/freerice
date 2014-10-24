e2e38ebb40953f229a68f751e1093fcbd8e6fdaa
Phodal HUANG <h@phodal.com>
2014-10-24 14:40:38 +0530

    Remove the unused lib

--- web/main.js
+++ web/main.js
@@ -3,19 +3,12 @@ require.config({
     paths: {
         'text': 'lib/text',
         jquery: 'lib/jquery-2.1.1.min',
-        async: 'lib/require/async',
         json: 'lib/require/json',
-        mdown: 'lib/require/mdown',
         router: 'router',
-        templates: 'templates',
-        jquerySidr: 'lib/jquery.sidr.min',
-        markdownConverter : 'lib/require/Markdown.Converter',
-        touchwipe: 'lib/jquery.touchwipe.min',
         underscore: 'lib/underscore',
         mustache: 'lib/mustache',
         backbone: 'lib/backbone',
-        "jquery-cookie": "lib/jquery.cookie",
-        q: 'lib/q'
+        "jquery-cookie": "lib/jquery.cookie"
     },
     shim: {
         jquerySidr:["jquery"],
