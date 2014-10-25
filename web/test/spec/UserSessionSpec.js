8898561ab5372fc31401888eac41e245b79f6a22
Phodal HUANG <h@phodal.com>
2014-10-25 23:04:25 +0530

    Add the first Jasmine test

--- web/test/spec/UserSessionSpec.js
+++ web/test/spec/UserSessionSpec.js
@@ -2,6 +2,7 @@ require([
   'jquery',
   '../../js/UserSession'
 ], function($, UserSession) {
+  'use strict';
   describe("User Session", function() {
     it("should can return a UserSession", function() {
       console.log('testing');
