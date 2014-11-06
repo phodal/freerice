b76a4f09789f8e3dcc4c8434b9647b9930b83f39
Fengda HUANG <h@phodal.com>
2014-11-06 16:32:13 +0530

    format the spec js

--- web/test/spec/UserSessionSpec.js
+++ web/test/spec/UserSessionSpec.js
@@ -1,29 +1,30 @@
 define([
-  'js/lib/UserSession'
+    'js/lib/UserSession'
 ], function(UserSession) {
-  'use strict';
-  describe("User Session", function() {
-    it("should return default name,accessToken be null", function() {
-      expect(UserSession.defaults.userName).toBe(null);
-      expect(UserSession.defaults.accessToken).toBe(null);
-    });
+    'use strict';
 
-    it("should be authenticated after save session", function() {
-      UserSession.save({
-        name: "test",
-        accessToken: "test"
-      });
-      expect(UserSession.authenticated()).toBe(true);
-    });
+    describe("User Session", function() {
+        it("should return default name,accessToken be null", function() {
+            expect(UserSession.defaults.userName).toBe(null);
+            expect(UserSession.defaults.accessToken).toBe(null);
+        });
 
-    it("should can load session after save session", function() {
-      UserSession.save({
-        name: "test",
-        accessToken: "test"
-      });
-      spyOn(UserSession, 'load');
-      UserSession.initialize();
-      expect(UserSession.load).toHaveBeenCalled();
+        it("should be authenticated after save session", function() {
+            UserSession.save({
+                name: "test",
+                accessToken: "test"
+            });
+            expect(UserSession.authenticated()).toBe(true);
+        });
+
+        it("should can load session after save session", function() {
+            UserSession.save({
+                name: "test",
+                accessToken: "test"
+            });
+            spyOn(UserSession, 'load');
+            UserSession.initialize();
+            expect(UserSession.load).toHaveBeenCalled();
+        });
     });
-  });
 });
