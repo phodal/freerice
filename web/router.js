05f26a67e7fddb00eb63ce6e99bceb6b2377634f
Fengda HUANG <h@phodal.com>
2014-11-11 22:22:24 +0530

    Refactor the router

--- web/router.js
+++ web/router.js
@@ -10,7 +10,6 @@ define([
     'js/lib/UserSession.js',
     'js/lib/Logout.js'
 ],function($, _, Backbone, HomeView, LoginView, CreateAccountView, User, UserProfileView, UserSession, Logout){
-
     var AppRouter = Backbone.Router.extend({
         routes: {
             'index': 'homePage',
@@ -21,55 +20,59 @@ define([
             'userProfile/*userName': 'userProfile',
             'account/logout': 'logout',
             '*actions': 'defaultAction'
-        }
-    });
-    window.app = new AppRouter();
-    var initialize = function() {
-        var router = new AppRouter;
-
-        router.on('route:homePage', function() {
+        },
+        index: function(){
             new HomeView();
-        });
-
-        router.on('route:Create', function() {
+        },
+        createAccount: function(){
             var createAccountView = new CreateAccountView();
             createAccountView.render();
-        });
-
-        router.on('route:Login', function() {
+        },
+        login: function(){
             var loginView = new LoginView();
             loginView.render();
-        });
-
-        router.on('route:logout', function() {
+        },
+        logout: function(){
             var logout = new Logout();
             logout.logout();
-            router.navigate('/', true)
-        });
-
-        router.on('route:admin', function(action) {
-            console.log(UserSession.authenticated());
+            this.navigate('/', true)
+        },
+        admin: function(){
             if(UserSession.authenticated() !=true ){
-                router.navigate('account/login', true)
+                this.navigate('account/login', true)
             }
-        });
-
-        router.on('route:userProfile', function(action) {
+        },
+        userProfile: function(){
             if (UserSession.authenticated() == true) {
                 var userProfileView = new UserProfileView();
                 userProfileView.render();
             } else {
-                router.navigate('account/login', true)
+                this.navigate('account/login', true)
             }
-        });
+        },
+        initialize: function() {
+            var router = this,
+                routes = [
+                    [ /^.*$/, "index" ],
+                    [ ":account/create", "createAccount" ],
+                    [ ":account/login", "login" ],
+                    [ ":account/logout", "logout" ],
+                    [ ":admin", "admin" ],
+                    [ ":userProfile", "userProfile"]
+                ];
 
-        router.on('route:defaultAction', function(actions){
-            new HomeView();
-            console.log('No route:', actions);
-        });
+            _.each(routes, function(route) {
+                router.route.apply(router,route);
+            });
+        }
+    });
 
+    var initialize = function() {
+        new AppRouter;
         Backbone.history.start();
     };
+
+    window.app = new AppRouter();
     return {
       initialize: initialize
     };
