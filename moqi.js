4fb62aa0cd9c8959bf679fd6634b4f19e91358d3
Fengda HUANG <h@phodal.com>
2014-11-20 17:41:47 +0800

    Add the freerice command line

--- moqi.js
+++ moqi.js
@@ -5,8 +5,12 @@ var program = require('commander')
 
 program
     .version(version)
-    .option('-o, --observe', 'Observe the given resource', 'boolean', false)
-    .option('-n, --no-new-line', 'No new line at the end of the stream', 'boolean', true)
-    .option('-p, --payload <payload>', 'The payload for POST and PUT requests')
-    .option('-q, --quiet', 'Do not print status codes of received packets', 'boolean', false)
-    .usage('[command] [options] url');
+    .option('-c, --create', 'Create Freerice project', 'boolean', false)
+    .usage('[command] [options] path');
+
+program.parse(process.argv);
+
+if (!program.args[0]) {
+    program.outputHelp();
+    process.exit(-1)
+}
